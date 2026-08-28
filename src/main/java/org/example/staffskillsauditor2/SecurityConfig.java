fa8c1b5c833194d3027cfadadf5700fbade1fcd1
phee <p037056n@student.staffs.ac.uk>
2026-08-28 18:05:38 +0100

    temp to test endpoints work

---
+++ src/main/java/org/example/staffskillsauditor2/SecurityConfig.java
@@ -0,0 +1,22 @@
+package org.example.staffskillsauditor2;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/skills/**").permitAll()
+                        .requestMatchers("/portfolio/**").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
